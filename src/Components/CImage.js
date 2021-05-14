b57dac42c0b893343db34fc394c4a74093fb3b21
Pranesh A <pranesh@divum.in>
2021-05-14 17:57:31 +0530

    changed icon size and react native base version and fixed issue in ios

--- src/Components/CImage.js
+++ src/Components/CImage.js
@@ -8,7 +8,7 @@ import CImageConstants from "../Constants/CImageConstants.js";
 import Fonts from "../Constants/Fonts.js";
 
 // const beeHRLogo = ({ children, ...otherProps }) => <Image source={CImageConstants.beeHRLogo} style={[{ height: Fonts.n(120), width: Fonts.n(120) }, otherProps.style]} {...otherProps} />;
-const beeHRSplashGif = ({ children, ...otherProps }) => <Image source={CImageConstants.beeHRSplashGif} style={[{ height: Dimensions.get('window').height-Fonts.n(200), width: Dimensions.get('window').width }, otherProps.style]} resizeMode={"cover"} {...otherProps} />;
+const beeHRSplashGif = ({ children, ...otherProps }) => <Image source={CImageConstants.beeHRSplashGif} style={[{ height: Fonts.n(250), width: Fonts.n(250) }, otherProps.style]} resizeMode={"contain"} {...otherProps} />;
 const login = ({ children, ...otherProps }) => <Image source={CImageConstants.login} style={[{ height: Fonts.n(125), width: Fonts.n(125) }, otherProps.style]} resizeMode={'cover'} {...otherProps} />;
 
 export default {
